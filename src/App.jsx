625ffd76eb4f3376f4d1f9a87f04e2e3338d20e2
Muhammad Moiz <moizm5410@gmail.com>
2026-05-06 15:56:24 +0500

    Removing Errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import PopularPage from "./pages/PopularPage"
